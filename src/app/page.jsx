83bebbc3a09710054394738df87a9c34a608992f
Anjan Shomodder <thatanjan@gmail.com>
2024-11-27 20:03:32 +0600

    Add finishing touch

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -20,8 +20,8 @@ export default function Home() {
   }, [isPending])
 
   return (
-    <div className=''>
-      <form className='max-w-xs' action={formAction}>
+    <div className='flex justify-center  items-center  h-screen'>
+      <form className='w-96' action={formAction}>
         <label className='form-control w-full'>
           <div className='label'>
             <span className='label-text'>Email</span>
@@ -52,38 +52,12 @@ export default function Home() {
 
         {error && (
           <div role='alert' className='alert alert-error'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-6 w-6 shrink-0 stroke-current'
-              fill='none'
-              viewBox='0 0 24 24'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'
-              />
-            </svg>
             <span>Error! {error}</span>
           </div>
         )}
 
         {message && (
           <div role='alert' className='alert alert-success'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-6 w-6 shrink-0 stroke-current'
-              fill='none'
-              viewBox='0 0 24 24'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'
-              />
-            </svg>
             <span>{message}</span>
           </div>
         )}
